60e7d433aca4db8345c02a64159cf0a3fcb2911b
Leskhan <63036210+Leskhan@users.noreply.github.com>
2020-11-05 13:58:03 +0600

    Add files via upload
    ACMP
    
    ЗАДАЧА №25
    Больше-меньше

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace STEPIK_C_SHARP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            if (a < b)
+                Console.WriteLine("<");
+            if (a > b)
+                Console.WriteLine(">");
+            if (a == b)
+                Console.WriteLine("");
+        }
+    }
+}
